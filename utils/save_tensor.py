0c4fa2ef84e82f80d5098c395e93c99d7dcb6ad1
Sebastian Zielinski <szielinski@uni-osnabrueck.de>
2024-04-29 14:31:04 +0200

    added save_tensor to this branch

---
+++ utils/save_tensor.py
@@ -0,0 +1,31 @@
+import struct
+from torch import Tensor
+
+
+
+def save_tensor(x: Tensor, data_path, filename):
+    with open(data_path + filename, "wb") as fp:
+        flattened_tensor = x.flatten()
+        for date in flattened_tensor:
+            fp.write(struct.pack("f", date.item()))
+    print("data written to", data_path + filename)
+    
+def save_tensor_to_hpp(x: Tensor, data_path: str, filename: str, array_name: str):
+    x_flat = x.flatten()
+    arr_len = int(list(x_flat.shape)[0])
+    header_text_top = "#ifndef MVIT_{}\n".format(array_name.upper())
+    header_text_top += "#define MVIT_{}\n\n".format(array_name.upper())
+    header_text_top += "namespace MVIT {\n\n"
+    header_text_top += "float {}[{}] = {{\n".format(array_name, arr_len)
+    
+    with open(data_path + filename, "wt") as fp:
+        cnt = 1
+        fp.write(header_text_top)
+        
+        for date in x_flat:
+            if cnt % 5 == 0:
+                fp.write("\n")
+            fp.write("{}, ".format(date.item()))
+            cnt += 1
+            
+        fp.write("\n};\n\n} // namespace MVIT\n\n#endif")
